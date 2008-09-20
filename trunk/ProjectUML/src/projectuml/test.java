77a01f6a97f0df28a5eb337b0d6399a5690b3cfd
steve.eriksson <steve.eriksson@766f0d02-85a6-11dd-90a9-6306d3ffb32f>
2008-09-20 14:13:33 +0000

    testar att commita en fil

---
+++ trunk/ProjectUML/src/projectuml/test.java
@@ -0,0 +1,22 @@
+/*
+ * test.java
+ *
+ * Created on den 20 september 2008, 16:13
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package projectuml;
+
+/**
+ *
+ * @author steer237
+ */
+public class test {
+    
+    /** Creates a new instance of test */
+    public test() {
+    }
+    
+}
