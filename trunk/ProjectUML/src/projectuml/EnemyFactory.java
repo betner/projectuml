4f48fac3abbe596fa47e6d54b0c51f20a4f887db
jens.turesson <jens.turesson@766f0d02-85a6-11dd-90a9-6306d3ffb32f>
2008-10-05 16:35:45 +0000

    Början till en fabrik, som tillverkar fiender vilket kan kännas lite gement men så är det i dagens våldsamma värld

--- trunk/ProjectUML/src/projectuml/EnemyFactory.java
+++ trunk/ProjectUML/src/projectuml/EnemyFactory.java
@@ -1,8 +1,8 @@
 
 package projectuml;
 
+import java.util.*;
 import java.awt.Point;
-import java.util.ArrayList;
 
 /**
  * EnemyFactory
@@ -13,6 +13,12 @@ import java.util.ArrayList;
  */
 public class EnemyFactory {
     
+    private Hashtable<String, EnemyCreator> creators;
+    
+    public EnemyFactory() {
+        creators = new Hashtable<String, EnemyCreator>();
+        creators.put("Default enemy", new DefaultEnemy());
+    }
     
     /**
      * Creates a default enemy
@@ -39,6 +45,24 @@ public class EnemyFactory {
         enemy.show();
         return enemy;
     }
+    
+    abstract private class EnemyCreator {
+        abstract public EnemyShip createEnemy(Path path);
+    }
+    
+    private final class DefaultEnemy extends EnemyCreator {
+        public EnemyShip createEnemy(Path path) {
+            ArrayList<Point> weaponMounts = new ArrayList<Point>();
+            weaponMounts.add(new Point(1, 20));
+            Gunner gunner = new CrazyGunner(500, 2000);
+            EnemyShip enemy = new EnemyShip(1, weaponMounts, gunner, path, "enemyship1.png");
+            enemy.addWeapon(new LaserCannon(false));
+            enemy.activate();
+            enemy.show();
+            return enemy;
+        }
+    }
+    
 }
 
 
