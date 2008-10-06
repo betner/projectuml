cc24b00582616705e5cad6fd90c5741776883caa
steve.eriksson <steve.eriksson@766f0d02-85a6-11dd-90a9-6306d3ffb32f>
2008-10-06 19:09:33 +0000

    Fixat kommentarer och kodlayout

--- trunk/ProjectUML/src/projectuml/Timestamp.java
+++ trunk/ProjectUML/src/projectuml/Timestamp.java
@@ -3,33 +3,36 @@ package projectuml;
 import java.io.*;
 
 /**
+ * TimeSStamp
+ * 
  * Records a marker in time and provides methods
- * for measuring how much time has passed since then
+ * for measuring how much time has passed since then.
  *
  * @author Jens Thuresson, Steve Eriksson
  */
 public class Timestamp implements Serializable {
 
     private long stamp;
-    
+
     /**
      * Creates the timestamp and marks the current
-     * elapsed time
+     * elapsed time.
      */
     public Timestamp() {
         reset();
     }
-    
+
     /**
-     * (Re)sets a marker in time here
+     * (Re)sets a marker in time here.
      */
     public void reset() {
         stamp = System.currentTimeMillis();
     }
-    
+
     /**
      * Checks if the specified amount of time have passed
-     * since the last set marker
+     * since the last set marker.
+     * 
      * @param milliseconds
      * @return True or false
      */
@@ -37,20 +40,21 @@ public class Timestamp implements Serializable {
         long diff = System.currentTimeMillis() - stamp;
         return diff >= milliseconds;
     }
-    
+
     /**
+     * Get amount of time passed since reset.
+     * 
      * @return Time passed in milliseconds
-     **/
+     */
     public long getTimePassed() {
         return System.currentTimeMillis() - stamp;
     }
-    
+
     /**
      * Serialize it as normal AND restart the timer
-     **/
+     */
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
         reset();
     }
-    
 }
