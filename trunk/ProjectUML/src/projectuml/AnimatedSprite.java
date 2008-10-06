d2cefcd3b2223aaaca3ac973d5e6a8be2a0a6526
steve.eriksson <steve.eriksson@766f0d02-85a6-11dd-90a9-6306d3ffb32f>
2008-10-06 17:16:48 +0000

    Fixat kommentarer och "layout"

--- trunk/ProjectUML/src/projectuml/AnimatedSprite.java
+++ trunk/ProjectUML/src/projectuml/AnimatedSprite.java
@@ -1,4 +1,3 @@
-
 package projectuml;
 
 import java.awt.image.BufferedImage;
@@ -10,123 +9,109 @@ import java.util.ArrayList;
  * AnimatedSprite
  *
  * Subclass of Sprite that hold a list of images and draws them to screen 
- * in a set time interval.
+ * in a given time interval.
  * All images should be added to imageList before it is activated.
- * By default Sprites are deactivated.
- *
+ * By default Sprites are deactivated and hidden.
+ * 
+ * @see Sprite
  * @author Steve Eriksson, Jens Thuresson
  */
 public class AnimatedSprite extends Sprite {
-   
-   private static final long serialVersionUID = 1L;
-   
-   private Timestamp time; // Timer
-   private long speed;     // Amount of time one image should be shown
-   private int runtime;    // Total running time of animation
-   private int sequence;   // Sequence number of image to draw
-   private  Boolean repeat; // Should animation repeat
-   private  Boolean done;   // Set to true when we don't repeat and we're done
-   transient private ArrayList<BufferedImage> imageList;  // Images to animate
-    
-    
+
+    private static final long serialVersionUID = 1L;
+    private Timestamp time; // Timer
+    private long speed;     // Amount of time one image should be shown
+    private int runtime;    // Total running time of animation
+    private int sequence;   // Sequence number of image to draw
+    private Boolean repeat; // Should animation repeat
+    private Boolean done;   // Set to true when we don't repeat and we're done
+    transient private ArrayList<BufferedImage> imageList;  // Images to animate
+
     /**
      * Create animated sprite and set default runtime
      */
-    public AnimatedSprite(){
+    public AnimatedSprite() {
         this(1000); // Run time set to one second
     }
-    
+
     /**
      * Create animated sprite and set default runtime and
      * make it not repeating
      *
      * @param runtime
      */
-    public AnimatedSprite(int runtime){
+    public AnimatedSprite(int runtime) {
         this(runtime, false); // Animation will not repeat
     }
-    
+
     /**
      * Create animated sprite
      * 
-     * @param runtime in milliseconds, repeat
+     * @param runtime in milliseconds
+     * @param repeat, true or false
      */
-    public AnimatedSprite(int runtime, Boolean repeat){
-        done = false;   // We haven't even started yet!
+    public AnimatedSprite(int runtime, Boolean repeat) {
+        done = false;
         this.runtime = runtime;
         this.repeat = repeat;
-        sequence = 0;   // Init to first entry in imageList
+        sequence = 0; // Point to first image in sequence
         time = new Timestamp();
         speed = 0;
         imageList = new ArrayList<BufferedImage>();
-        
-        /*System.out.println("AnimatedSprite created:");
-        System.out.println("=> Speed: " + speed);
-        System.out.println("=> Done flag:" + done.toString());
-        System.out.println("=> Runtime: " + runtime);*/
     }
+
     /**
      * Add image to the list of images to be animated
-     * and update animation speed.
+     * and update the animation speed.
      */
-    public void addImage(BufferedImage image){
+    public void addImage(BufferedImage image) {
         imageList.add(image);
-        speed = runtime / imageList.size();   
-        
-        /*System.out.println("AnimatedSprite: addImage()");
-        System.out.println("=> imageList.size(): " + imageList.size());
-        System.out.println("=> runtime: " + runtime);
-        System.out.println("=> New speed set to: " + speed);*/
+        speed = runtime / imageList.size();
     }
 
     /**
      * Update state of animation.
-     * Set current image, Sprite's image, to the next
-     * image in the sequence. 
-     * If animation isn't looped future updates will be ignored
-     * when we reach the end of the sequence.
+     * Change image if the configured time has passed.
+     * If the animation isn't looped future updates will be 
+     * ignored when it reaches the end of the sequence.
+     * 
+     * @param level reference which isn't used by thos object
      */
     public void update(Level level) {
-        if(isActive()){
+        if (isActive()) {
             // Current picture should be changed if the time
             // set in speed has passed since last update
-            if(time.havePassed(speed)){ 
+            if (time.havePassed(speed)) {
                 setImage(imageList.get(sequence % imageList.size()));
                 sequence++;   // Point to next image
                 time.reset(); // Reset timestamp
-                
-                System.out.println("AnimatedSprite: update()");
-                System.out.println("=> time passed");
             }
             // We've gone through the image sequence and should
             // not loop so make sure no image is displayed
-            if(!repeat && sequence >= imageList.size()){
+            if (!repeat && sequence >= imageList.size()) {
                 hide();
                 deactivate();
                 done = true; // Animation is done!
-                
-                System.out.println("AnimatedSprite: done = true");
             }
         }
     }
-   
+
     /**
-     * Is animation done?
+     * Returns true if the animation is done.
      *
      * @return true or false
      */
-    public Boolean isDone(){
+    public Boolean isDone() {
         return done;
     }
-    
+
     /**
      * Reset animation
      */
-    public void reset(){
+    public void reset() {
         sequence = 0; // First image in sequence
-        done = false; // 
-        activate();   //
+        done = false;
+        activate();
         show();
     }
- 
 }
